7185a1aacdb8c46cf17b557ee788e8e2ee1b60f5
masaru ogawa <masaru@gmail.com>
2023-09-20 13:13:07 +0900

    ft_echo.h added to commit

---
+++ includes/ft_echo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 11:10:51 by mogawa            #+#    #+#             */
+/*   Updated: 2023/09/20 12:59:19 by mogawa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ECHO_H
+# define FT_ECHO_H
+
+# include <stdbool.h>
+# define OPTION_LOC (0)//! depends on whether passing command name
+
+void	ft_echo(char **argv);
+
+#endif
