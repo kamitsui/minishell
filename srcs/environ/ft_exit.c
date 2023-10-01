71172e94c8f43d6d26f8da103d9bb2955a2cbed5
masaru ogawa <masaru@gmail.com>
2023-10-01 16:24:22 +0900

    env_delete_t_envwrap function in env_utils.c created. ft_exit in ft_exit.c created. Fixed #9.

--- srcs/environ/ft_exit.c
+++ srcs/environ/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 19:58:19 by mogawa            #+#    #+#             */
-/*   Updated: 2023/10/01 14:30:58 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/10/01 16:22:49 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	ft_exit(int status, t_envwrap *env)
 {
-	env->exit_code = status;
+	env_delete_t_envwrap(env);
+	//? statusによってEXITｈ出力先をｈするあ必要性はあるだろうか？
+	if (status == EXIT_SUCCESS)
+		ft_putstr_fd("exit\n", STDOUT_FILENO);
+	else
+		ft_putstr_fd("exit\n", STDERR_FILENO);
 	exit(status);
 }
