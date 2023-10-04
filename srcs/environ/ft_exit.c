cd218369c18e9b3b0b2a8af62acb24bb534209c3
masaru ogawa <masaru@gmail.com>
2023-10-04 14:24:57 +0900

    ft_exit.c exit\n to STDERR by changing ft_putstr_fd fixed #35.

--- srcs/environ/ft_exit.c
+++ srcs/environ/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 19:58:19 by mogawa            #+#    #+#             */
-/*   Updated: 2023/10/01 16:31:12 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/10/04 14:24:22 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 void	ft_exit(int status, t_envwrap *env_wrap)
 {
 	env_delete_t_envwrap(env_wrap);
-	//? statusによってEXITｈ出力先をｈするあ必要性はあるだろうか？
-	if (status == EXIT_SUCCESS)
-		ft_putstr_fd("exit\n", STDOUT_FILENO);
-	else
-		ft_putstr_fd("exit\n", STDERR_FILENO);
+	ft_putstr_fd("exit\n", STDERR_FILENO);
 	exit(status);
 }
