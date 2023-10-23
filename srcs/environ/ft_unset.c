44c60132418f5cce3cb7b4e8baf37e0b298d73a5
masaru ogawa <masaru@gmail.com>
2023-10-24 08:47:10 +0900

    バグFIX 内容：ft_unsetにおいて最後の項目を削除するときにセグフォが発生 bag cause: inside ft_unset,  was done without checking bag fix: inside ft_unset, if (next != NULL) added

--- srcs/environ/ft_unset.c
+++ srcs/environ/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:49:33 by mogawa            #+#    #+#             */
-/*   Updated: 2023/09/25 11:18:04 by mogawa           ###   ########.fr       */
+/*   Updated: 2023/10/24 08:40:02 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,22 @@ void	ft_unset(t_envwrap *env_wrap, char *tgt_key)
 {
 	t_list	*crnt;
 	t_list	*next;
+	t_list	*prev;
 	t_env	*env_node;
 
 	crnt = env_wrap->env->next;
 	while (crnt)
 	{
 		next = crnt->next;
+		prev = crnt->prev;
 		env_node = crnt->content;
 		if (ft_strcmp(env_node->key, tgt_key) == 0)
 		{
-			crnt->prev->next = crnt->next;
-			crnt->next->prev = crnt->prev;
+			prev->next = next;
+			if (next != NULL)
+				next->prev = prev;
+			// crnt->prev->next = crnt->next;
+			// crnt->next->prev = crnt->prev;
 			ft_lstdelone(crnt, _env_del_content);
 		}
 		crnt = next;
