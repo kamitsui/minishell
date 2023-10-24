41cb6c2a16ce0387186900def730db213e184259
Kaishiro Mitsui <kaishiro.mitsui@gmail.com>
2023-10-24 16:27:42 +0900

    Fix bug ( if command is only "echo" )

--- srcs/builtins/call_echo.c
+++ srcs/builtins/call_echo.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:19:41 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/10/21 17:20:45 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/10/24 16:26:29 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,31 @@ int	call_echo(t_ast *node, t_envwrap *env_wrapper)
 {
 	char	**argv;
 	size_t	i;
+	size_t	size;
 
 	(void)env_wrapper;
-	argv = (char **)malloc(sizeof(char *) * (node->num_children + 1));
-	i = 0;
-	while (i < node->num_children)
+	if (node->num_children == 0)
+		size = 1;
+	else
+		size = node->num_children;
+	argv = (char **)malloc(sizeof(char *) * (size + 1));
+	if (node->num_children == 0)
 	{
-		argv[i] = node->children[i]->value;
-		i++;
+		argv[0] = "";
+		argv[1] = NULL;
+		ft_echo(argv);
 	}
-	argv[i] = NULL;
-	ft_echo(argv);
+	else
+	{
+		i = 0;
+		while (i < node->num_children)
+		{
+			argv[i] = node->children[i]->value;
+			i++;
+		}
+		argv[i] = NULL;
+		ft_echo(argv);
+	}
 	free(argv);
 	// ここではargv[i]をfreeせず、
 	// 大元のlets_go_shell関数でnode->children[i]->valueをまとめてフリーする
