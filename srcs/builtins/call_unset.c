7f25c4f057ebe396ae040b65ffe4acfae1183e53
Kaishiro Mitsui <kaishiro.mitsui@gmail.com>
2023-10-24 14:26:08 +0900

    Fix bug ( if command is only "unset" )

--- srcs/builtins/call_unset.c
+++ srcs/builtins/call_unset.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:17:56 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/10/21 17:23:46 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/10/24 14:23:38 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	call_unset(t_ast *node, t_envwrap *env_wrapper)
 //	if (is_error_unset(...) == true)
 //		handl_error_not_exit(...);
 //		return (EXIT_FAILURE);
+	if (node->num_children == 0)
+		return (EXIT_SUCCESS);
 	tgt_key = node->children[0]->value;
 	ft_unset(env_wrapper, tgt_key);
 	return (EXIT_SUCCESS);
