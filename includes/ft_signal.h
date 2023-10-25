4622b25ffa3c3bc4f97d82b0bfca9317d842e1a3
masaru ogawa <masaru@gmail.com>
2023-10-25 14:55:23 +0900

    heredoc signal added

--- includes/ft_signal.h
+++ includes/ft_signal.h
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 14:22:48 by mogawa            #+#    #+#             */
-/*   Updated: 2023/10/21 14:54:37 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:46:07 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 
 # include <signal.h>
 # include <stdlib.h>
+# include <stdbool.h>
 
 typedef struct sigaction	t_sigaction;
 volatile sig_atomic_t		g_flag;
 
-void	sig_signal_initializer(t_sigaction *act, int sig_type);
+void	sig_signal_initializer(t_sigaction *act, int sig_type, bool in_heredoc);
 
 #endif
