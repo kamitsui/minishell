46434b5b3d87d3de4f34bb6f926a7d57ac3d0a34
Kaishiro Mitsui <kaishiro.mitsui@gmail.com>
2023-11-06 15:08:09 +0900

    Fix zombie process ( cat << eof | ls )

--- srcs/execute_utils/wait_process.c
+++ srcs/execute_utils/wait_process.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 20:07:43 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/10/26 17:47:11 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/11/06 15:06:40 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ int	wait_process(pid_t pid, int num_commands)
 	int	i;
 	int	status;
 
+	waitpid(pid, &status, 0);
 	i = 0;
 	while (i < num_commands - 1)
 	{
 		waitpid(-1, &status, 0);
 		i++;
 	}
-	waitpid(pid, &status, 0);
 	if (WIFEXITED(status))
 		status = WEXITSTATUS(status);
 	else if (WIFSIGNALED(status))
