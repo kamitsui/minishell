c3968bb23ff4a503c7fff263ba1a976df088dddb
Kaishiro Mitsui <kaishiro.mitsui@gmail.com>
2023-11-09 06:00:33 +0900

    Fix bug : terms of EOF

--- srcs/traverse_utils/here_doc_utils/input_from_stdin.c
+++ srcs/traverse_utils/here_doc_utils/input_from_stdin.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 05:12:08 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/11/09 05:13:44 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/11/09 05:58:12 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@
 void	input_from_stdin(int fd, char *end_of_block)
 {
 	char	*line;
+	size_t	len;
 
 	write(STDIN_FILENO, PROMPT_HERE_DOC, ft_strlen(PROMPT_HERE_DOC));
 	line = get_next_line(STDIN_FILENO);
 	while (line != NULL)
 	{
-		if (ft_strncmp(line, end_of_block, ft_strlen(line) - 1) == 0)
+		len = ft_strlen(end_of_block);
+		if (ft_strncmp(line, end_of_block, len - 1) == 0
+			&& line[len] == '\n')
 			break ;
 		write(fd, line, ft_strlen(line));
 		free(line);
