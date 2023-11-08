c08aeb5b5c929b7f67f53cfebfa2f773138533e4
Masaru Ogawa <mogawa@c1r15s5.42tokyo.jp>
2023-11-08 16:27:05 +0900

    fix: env when env -i ./minishell

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mogawa <mogawa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 20:41:05 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/10/29 12:50:34 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/11/08 16:21:51 by mogawa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,35 @@
 #include "minishell.h"
 #include "execute.h"
 #include "free_minishell.h"
+#include "error_minishell.h"
+
+static char	**get_alter_env(void)
+{
+	char	**dummy_env;
+	char	*pwd;
+	int		i;
+
+	dummy_env = ft_calloc(5, sizeof(char *));
+	if (!dummy_env)
+		ft_errno_exit("ft_calloc");
+	dummy_env[0] = ft_strdup("OLDPWD");
+	dummy_env[1] = ft_strdup("SHLVL=1");
+	dummy_env[2] = ft_strdup(\
+			"PATH=/usr/gnu/bin:/usr/local/bin:/bin:/usr/bin:.");
+	pwd = getcwd(NULL, 0);
+	dummy_env[3] = ft_strdup(ft_strjoin("PWD=", pwd));
+	free(pwd);
+	i = 0;
+	while (i < 3)
+	{
+		if (dummy_env[i] == NULL)
+			ft_errno_exit("ft_strdup");
+		i++;
+	}
+	dummy_env[4] = NULL;
+	return (dummy_env);
+}
+
 
 /**
  * @brief minishellの入口　環境変数のデータをinput関数へ渡して、
@@ -34,7 +63,10 @@ int	main(int argc, char *argv[], char *env[])
 {
 	int			status;
 	t_envwrap	*env_wrapper;
+	char		**environ;
 
+	if (*env == NULL)
+		env = get_alter_env();
 	env_wrapper = create_env_list(env);
 	if (argc == 1)
 		status = input(env_wrapper);
