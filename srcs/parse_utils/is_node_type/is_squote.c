532c8191d3ebd78d963e5d52fd8a38ea23c480e0
Kaishiro Mitsui <kaishiro.mitsui@gmail.com>
2023-11-08 19:14:31 +0900

    Fix bug of single quote expansion ( echo '$USER'"'" )

--- srcs/parse_utils/is_node_type/is_squote.c
+++ srcs/parse_utils/is_node_type/is_squote.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 01:29:16 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/10/14 02:35:01 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/11/08 18:22:26 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,22 @@
 
 bool	is_squote(const char *token)
 {
-	size_t	count;
+	size_t	count_squote;
+	size_t	count_dquote;
 	size_t	i;
 
-	count = 0;
+	count_squote = 0;
+	count_dquote = 0;
 	i = 0;
 	while (token[i] != '\0')
 	{
-		if (token[i] == META_SQUOT_CHR)
-			count++;
+		if (count_squote == 0 && token[i] == META_DQUOT_CHR)
+			count_dquote++;
+		if ((count_dquote % 2) == 0 && token[i] == META_SQUOT_CHR)
+			count_squote++;
+		if (count_squote == 2)
+			return (true);
 		i++;
 	}
-	if (count == 0)
-		return (false);
-	return (count % 2 == 0);
+	return (false);
 }
